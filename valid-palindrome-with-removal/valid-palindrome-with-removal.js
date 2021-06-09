d86b03a85a299c411a6c2b65e73d85d4566bd7d2
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-09 09:09:05 -0400

    completed OICE

---
+++ valid-palindrome-with-removal/valid-palindrome-with-removal.js
@@ -0,0 +1,19 @@
+/*
+This question is asked by Facebook. Given a string and the ability to delete at most one character, return whether or not it can form a palindrome.
+Note: a palindrome is a sequence of characters that reads the same forwards and backwards.
+
+Ex: Given the following strings...
+
+"abcba", return true
+"foobof", return true (remove the first 'o', the second 'o', or 'b')
+"abccab", return false
+
+OICE
+
+Output: a boolean => represent whether is a palindrome with a removal
+Input: a string
+Contraints: logn
+Edge Cases: empty string => true, type must be string
+
+
+*/
