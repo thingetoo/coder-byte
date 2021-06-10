ec9a971e45fd205e865ed91e3044995bab9b89c6
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-10 09:05:06 -0400

    Completed the OICE

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -1,3 +1,19 @@
 /*
+This question is asked by Google. Given an array of integers, return whether or not two numbers sum to a given target, k.
+Note: you may not sum a number with itself.
+
+Ex: Given the following...
+
+[1, 3, 8, 2], k = 10, return true (8 + 2)
+[3, 9, 13, 7], k = 8, return false
+[4, 2, 6, 5, 2], k = 4, return true (2 + 2)
+
+OICE method:
+
+Output: a boolean that represents whether or not two numbers sum to the target
+Input: an array of numbers
+Constraints: less than O(n) time
+Edge Cases: if the array length is equal to one, return false,
+
 
 */
