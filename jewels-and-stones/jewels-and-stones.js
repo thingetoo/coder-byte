d8e2406e5e420c8a75020bd3f18a6594223f28dc
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-11 09:13:31 -0400

    Added pseudocode

---
+++ jewels-and-stones/jewels-and-stones.js
@@ -0,0 +1,30 @@
+// This question is asked by Amazon. Given a string representing your stones and another string representing a list of jewels, return the number of stones that you have that are also jewels.
+
+// Ex: Given the following jewels and stones...
+
+// jewels = "abc", stones = "ac", return 2
+// jewels = "Af", stones = "AaaddfFf", return 3
+// jewels = "AYOPD", stones = "ayopd", return 0
+
+// OICE
+// Output: a number representing the stones that are also jewels
+// Input: two strings representing the jewels and stones
+// Constraints: O(n) or less
+// Edge Cases: n/a
+
+// declare a result variable which will keep track of the # of stones that are also jewels
+// initialize a hash => will contain the jewels eventually
+
+// iterate over the jewels
+//   store them in a hash table: if not seen store them, => quantity doesn't matter
+
+
+// iterate over the stones
+//   check if each stone is present in the jewels hash
+//     increment result
+
+// return the result
+
+const jewelsAndStones = (jewels, stones) => {
+  
+}
