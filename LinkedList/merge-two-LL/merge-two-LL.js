0347b760f79394002553ecc169827d9a9a582ddf
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-17 08:52:38 -0400

    OICE method

--- LinkedList/merge-two-LL/merge-two-LL.js
+++ LinkedList/merge-two-LL/merge-two-LL.js
@@ -6,4 +6,15 @@ Ex: Given the following lists...
 list1 = 1->2->3, list2 = 4->5->6->null, return 1->2->3->4->5->6->null
 list1 = 1->3->5, list2 = 2->4->6->null, return 1->2->3->4->5->6->null
 list1 = 4->4->7, list2 = 1->5->6->null, return 1->4->4->5->6->7->null
+
+OICE:
+Output: A LL that is sorted in ascending order
+Input: Two LL
+Constraints: each list is sorted
+Edge Cases: If one list is null return the other
+
+if one list is null
+  return other one
+
+
 */
