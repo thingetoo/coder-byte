7d0bed472b7e6f93c81f67521488753fa65401cc
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-17 08:57:48 -0400

    ompleted pseudo code

--- LinkedList/merge-two-LL/merge-two-LL.js
+++ LinkedList/merge-two-LL/merge-two-LL.js
@@ -16,5 +16,20 @@ Edge Cases: If one list is null return the other
 if one list is null
   return other one
 
+check which linked list has the lower value => this will be the head
+declare a variable of a new linked list with the lowest value => merged
+keep a reference to the head
+
+iterate through the lists => while loop
+  check for the smallest value
+  create a new node for that value
+  add it to the list
+
+check which list still has values
+  add that list to the end
+return head
+
+l1 = [1,2,4], l2 = [1,3,4]
+
 
 */
