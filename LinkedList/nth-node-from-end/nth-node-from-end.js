ce72f2d2a46ca36bc2846d8c3a2121480e6d287f
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-18 09:05:58 -0400

    OICE writeen out

--- LinkedList/nth-node-from-end/nth-node-from-end.js
+++ LinkedList/nth-node-from-end/nth-node-from-end.js
@@ -2,4 +2,22 @@
 This question is asked by Facebook.
 
 Given a linked list and a value n, remove the nth to last node and return the resulting list.
+
+TEST:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+OICE
+Output: return a LL with the value removed
+Input: a LL and a number
+Constraints: n is always in range
+Edge Cases: removing from the start => setting a new head, having one value => return head.next
 */
