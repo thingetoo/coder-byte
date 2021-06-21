057e239fa6baf45522a9292543705d4407c48913
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-21 12:23:30 -0400

    FInished prob

--- LinkedList/middle-value/middle value.js
+++ LinkedList/middle-value/middle value.js
@@ -1,3 +1,4 @@
+/*
 This question is asked by Amazon. Given a non-empty linked list, return the middle node of the list. If the linked list contains an even number of elements, return the node closer to the end.
 Ex: Given the following linked lists...
 
@@ -16,3 +17,11 @@ Edge Cases: if node only has length of 1 => return it
 
 HL: have two pointer, one move fast and the other move slow
 
+Pseudo:
+
+1. declare a fast and slow pointer set equal to the head
+2. while fast and fast.next are not null
+      - iterate the slow by one step and the fast by two steps
+
+3. return the slow pointer
+*/
