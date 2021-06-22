17d50713f35591ae52cbdbb76aff72a1e0e2c4aa
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-22 09:11:00 -0400

    OICE complete

---
+++ LinkedList/start-of-cycle/start-of-cycle.js
@@ -0,0 +1,17 @@
+/*
+This question is asked by Apple. Given a potentially cyclical linked list where each value is unique, return the node at which the cycle starts. If the list does not contain a cycle, return null.
+
+Ex: Given the following linked lists...
+
+1->2->3, return null
+1->2->3->4->5->2 (5 points back to 2), return a reference to the node containing 2
+1->9->3->7->7 (7 points to itself), return a reference to the node containing 7
+
+OICE:
+Output: A reference to the node where the cycle occurs
+Input: a LL
+Constraints: n/a
+Edge Cases: a node pointing to itself
+
+PSEUDO
+*/
