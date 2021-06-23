f29b0c10bb696bc14d421a278b55a3f0898144e3
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-23 09:41:37 -0400

    Reverse list code complete

--- LinkedList/reverse-list/reverse-list.js
+++ LinkedList/reverse-list/reverse-list.js
@@ -14,7 +14,7 @@ Constraints: n/a
 Edge Cases: if null or node.next is null, return the input
 
 7->15->9->2->null
-          ^
+   ^
 
 
 prev = 7->null
@@ -33,6 +33,28 @@ next = null
 2->9->15->7->null
 1. we want to get a reference of the next val => rest of the LL
 2. if its the head, we want to set the head val to null
-3.
+3. loop through the linked list while current is not null
+  - set next equal to the current.next
+  - set the current.next equal to the prev
+  - set the prev equal to the current
+  - the current equal to the next
 
 */
+
+var reverseList = function (head) {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  let current = head;
+  let prev = current;
+  prev.next = null;
+  current = current.next;
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
