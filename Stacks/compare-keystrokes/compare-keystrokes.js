e356f5cbf92e61a1cab489dae3e1d25ff2aaccd7
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-25 15:23:25 -0400

    final

--- Stacks/compare-keystrokes/compare-keystrokes.js
+++ Stacks/compare-keystrokes/compare-keystrokes.js
@@ -20,4 +20,34 @@ loop over both strings => (x2)
 convert both of the arrays to strings
 - join on empty string (x2)
 coompare the two strings for equality
+
+s = "ABC#", t = "CD##AB", return true
+
+s ['A', 'B'] => 'AB'
+t ['A', 'B'] => 'AB'
 */
+
+var backspaceCompare = function (s, t) {
+  const sStack = [];
+  const tStack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '#') {
+      sStack.pop();
+    } else {
+      sStack.push(s[i]);
+    }
+  }
+  for (let i = 0; i < t.length; i++) {
+    if (t[i] === '#') {
+      tStack.pop();
+    } else {
+      tStack.push(t[i]);
+    }
+  }
+
+  const sStringed = sStack.join('');
+  const tStringed = tStack.join('');
+
+  return sStringed === tStringed;
+};
