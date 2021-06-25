48e508ec6e283cdbb8ce8648a07986ad34f94cf1
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-25 13:42:12 -0400

    Compare keystrokes prob pseudo

--- Stacks/compare-keystrokes/compare-keystrokes.js
+++ Stacks/compare-keystrokes/compare-keystrokes.js
@@ -7,4 +7,17 @@ s = "cof#dim#ng", t = "code", return false
 
 
 HL: create two stacks that will contain the current letters and pop letters off the stack when we encounter a backspace
+  - at the end compare the two stacks => join and compare equality
+
+declare two stacks - one for each string
+
+loop over both strings => (x2)
+  - if its a letter
+    - we add it to the stack
+  - else (a backspace)
+    - then we pop it off the stack
+
+convert both of the arrays to strings
+- join on empty string (x2)
+coompare the two strings for equality
 */
