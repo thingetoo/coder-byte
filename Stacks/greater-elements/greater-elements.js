ba898844343239eb834b0e6082ce9f1fbe9bad10
Brandon Galloway <brandon.galloway2@gmail.com>
2021-06-27 09:17:47 -0400

    completed the psuedo

--- Stacks/greater-elements/greater-elements.js
+++ Stacks/greater-elements/greater-elements.js
@@ -5,4 +5,6 @@ Ex: Given the following arrays…
 
 nums1 = [4,1,2], nums2 = [1,3,4,2], return [-1, 3, -1] because no element in nums2 is greater than 4, 3 is the first number in nums2 greater than 1, and no element in nums2 is greater than 2.
 nums1 = [2,4], nums2 = [1,2,3,4], return [3, -1] because 3 is the first greater element that occurs in nums2 after 2 and no element is greater than 4.
+
+
 */
