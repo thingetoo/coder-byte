4b1439c0a1a16f0fbbbd45140d9cc4a40c0a8a35
Brandon Galloway <brandon.galloway2@gmail.com>
2021-07-01 09:12:59 -0400

    OICE and pseudo

--- Trees/find-value/find-value.js
+++ Trees/find-value/find-value.js
@@ -1,4 +1,15 @@
 /*
 This question is asked by Google. Given the reference to the root of a binary search tree and a search value, return the reference to the node that contains the value if it exists and null otherwise.
 Note: all values in the binary search tree will be unique.
+
+OICE
+Output: a tree node
+Input: the root of a tree
+Constraints: n/a
+Edge Cases: if tree is null return null
+
+
+HL: compare the node value to the value we're looking for => recurse: call func or left child, call function on right right child
+
 */
+
