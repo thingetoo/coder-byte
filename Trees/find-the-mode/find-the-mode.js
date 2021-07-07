dc704b77c338e8aa5ef4d0aca73b2140a97f47a0
Brandon Galloway <brandon.galloway2@gmail.com>
2021-07-07 09:05:29 -0400

    New code

---
+++ Trees/find-the-mode/find-the-mode.js
@@ -0,0 +1,3 @@
+/*
+Given a binary search tree, return its mode (you may assume the answer is unique). If the tree is empty, return -1. Note: the mode is the most frequently occurring value in the tree
+*/
---
+++ Trees/minimum-difference/minimum-difference.js
@@ -0,0 +1,16 @@
+/*
+OICE
+
+Output: a number => rep the min difference
+Input: a tree node => rep the root
+Constraints: n/a
+Edge Cases: null => return null
+
+*/
+
+const minDiff = (tree) => {
+  let min = Infinity;
+  function inner(t, min) {
+
+  }
+}
